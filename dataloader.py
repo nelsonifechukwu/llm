53ab579184263f5e00766e11c6df0c592d2ee015
nelsonifechukwu <nelson.ifechukwu@gmail.com>
2026-05-10 13:56:36 +0100

    feat: added data loader

---
+++ dataloader.py
@@ -0,0 +1,65 @@
+import tiktoken
+import torch
+from torch.utils.data import DataLoader, Dataset
+
+with open("verdict.txt", "r", encoding="utf-8") as f:
+    verdict = f.read()
+
+# enc_text = tokenizer.encode(verdict)
+# enc_sample = enc_text[100:]
+
+# context_size = 4 # this is similar to Claude's context window--the number of tokens the model considers when making predictions, allowing it to understand and generate coherent text based on that context.
+# x = enc_sample[:context_size]
+# y = enc_sample[1:context_size+1]
+
+# # pair of input and target sequences
+# for i in range(context_size):
+#     print(tokenizer.decode(x[:i+1]), "----->", tokenizer.decode([y[i]]))
+
+
+class CustomDataset(Dataset):
+    def __init__(self, text, tokenizer, context_size, stride):
+        self.token_ids = tokenizer.encode(text)
+        self.input_ids = []
+        self.target_ids = []
+        for i in range(0, len(self.token_ids) - context_size, stride):
+            input_chunk = self.token_ids[i : i + context_size]  # [a,b,c]
+            target_chunk = self.token_ids[i + 1 : i + context_size + 1]  # [b,c,d]
+            self.input_ids.append(torch.tensor(input_chunk))
+            self.target_ids.append(torch.tensor(target_chunk))
+
+    def __len__(self):
+        return len(self.input_ids)
+
+    def __getitem__(self, idx):
+        return self.input_ids[idx], self.target_ids[idx]
+
+
+def create_dataloader(
+    text,
+    context_size=256,
+    stride=1,
+    batch_size=4,
+    shuffle=True,
+    drop_last=True,
+    num_workers=0,
+):
+    tokenizer = tiktoken.get_encoding("gpt2")
+    dataset = CustomDataset(text, tokenizer, context_size, stride)
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        drop_last=drop_last,
+        num_workers=num_workers,
+    )
+    return dataloader
+
+
+dataloader = create_dataloader(
+    verdict, batch_size=1, context_size=5, stride=5, shuffle=False
+)
+data_iter = iter(dataloader)
+first_batch = next(data_iter)
+second_batch = next(data_iter)
+print(torch.stack(first_batch), torch.stack(second_batch))
