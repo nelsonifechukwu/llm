7b511433b556fa2005f8b24bef8a993abcca7aaa
nelsonifechukwu <nelson.ifechukwu@gmail.com>
2026-06-19 11:15:32 +0100

    feat: finished adding positional embeddings

--- embedding.py
+++ embedding.py
@@ -6,7 +6,10 @@ with open("verdict.txt", "r", encoding="utf-8") as f:
     
 
 #load tokens
-data_loader = create_dataloader(verdict, 5, 5, 1, False, True)
+batch_size = 8
+context_length = stride = 4
+
+data_loader = create_dataloader(verdict, context_length, stride, batch_size, False, True)
 data_loader = iter(data_loader)
 input, target = next(data_loader)
 
@@ -19,15 +22,27 @@ embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
 #tokens -> embeddings (similar to one-hot encoding)
 token_embeddings = embedding_layer(input)
 
-"""
-[one-hot vector][embedding matrix]    
-                [embedding matrix]
-                [embedding matrix]
-"""
+
+#[one-hot vector][embedding matrix]    
+#                [embedding matrix]
+#                [embedding matrix]
+
 one_hot = torch.nn.functional.one_hot(input.squeeze(dim=0), vocab_size)
 linear = torch.nn.Linear(vocab_size, output_dim, bias=False)
 linear.weight = torch.nn.Parameter(embedding_layer.weight)
 output = one_hot.float() @ linear.weight #similar to linear(one_hot.float())
-print(output)
-print(token_embeddings)
+#print(output) print(token_embeddings) -> should be the same thing
 
+
+#GPT absolute embedding approach
+pos_embedding_layer = torch.nn.Embedding(context_length, output_dim)
+pos_embeddings = pos_embedding_layer(torch.arange(context_length)) #use a placeholder var 
+
+#N.b: embedding_layer(input) is like a lookup, not like [vector][matrix] mul in the Linear layer
+
+#add the pos_embeddings to the token embeddings
+input_embeddings = token_embeddings + pos_embeddings #python broadcasting
+
+if __name__ == "__main__":
+    print(input_embeddings)
+    print(input_embeddings.shape)
