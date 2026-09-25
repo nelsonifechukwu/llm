fc357ce27b21b5e7d90ba628de93aed9568d8713
nelsonifechukwu <nelson.ifechukwu@gmail.com>
2026-09-25 23:06:17 +0100

    computed  attn score for all inputs wrt q_1

--- self_attention.py
+++ self_attention.py
@@ -9,13 +9,21 @@ W_q = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False) #256 x 12
 W_k = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
 W_v = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
 
-q_1 = x_1 @ W_q
+q_1 = x_1 @ W_q #1 x 128
 k_1 = x_1 @ W_k
 v_1 = x_1 @ W_v
 
 # obtain all KV pairs for all inputs
-keys = input_embeddings @ W_k
+keys = input_embeddings @ W_k # 8 x 4 x 128
 values = input_embeddings @ W_v
 
+#compute attn_scores for all inputs wrt q_1
+attn_scores_q1 = keys @ q_1
+dim_k = keys.shape[-1]
+original_shape = attn_scores_q1.shape
+attn_scores_q1 = attn_scores_q1.reshape(1, -1)
+scaled_attn_weights_q1 = torch.softmax(attn_scores_q1/dim_k**0.5, dim = 1)
+scaled_attn_weights_q1 = scaled_attn_weights_q1.reshape(original_shape)
 
-print(keys.shape)
+
+print(scaled_attn_weights_q1.shape)
